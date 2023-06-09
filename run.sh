bc63bdc88f05d36163f963e66ca4fef2a634af67
Vian Chen <vian-business@outlook.com>
2023-06-09 18:09:22 +0800

    add: correct start script

--- run.sh
+++ run.sh
@@ -1,12 +1,2 @@
-echo "this is NODE-$NODE_ID"
-if [[ "$NODE_ID" == "1" ]]; then
-	# 启动节点1对应服务
-  sleep 1000;
-else
-	# 启动其他节点对应服务
-  sleep 1000;
-fi
-
-
-
-
+echo "this is node $NODE_ID out of $NODE_NUM"
+cd mamaserving && ./server
